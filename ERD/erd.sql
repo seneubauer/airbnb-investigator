214de48cbcfb8b38d00d4e66f8e4cc0f4ba5a54a
Thomas Lenzmeier <thomas.lenzmeier@constellationmutual.com>
2022-10-06 15:06:13 -0500

    Finished writing code for insertion of csv data into stage tables.

--- ERD/erd.sql
+++ ERD/erd.sql
@@ -4,9 +4,9 @@
 
 CREATE TABLE "airports" (
     "iata" varchar(4)   NOT NULL,
-    "airport_name" varchar(255)   NOT NULL,
-    "city" varchar(255)   NOT NULL,
-    "state" varchar(2)   NOT NULL,
+    "airport_name" varchar(255)   NULL,
+    "city" varchar(255)   NULL,
+    "state" varchar(2)   NULL,
     "latitude" decimal   NOT NULL,
     "longitude" decimal   NOT NULL,
     CONSTRAINT "pk_airports" PRIMARY KEY (
@@ -16,27 +16,27 @@ CREATE TABLE "airports" (
 
 CREATE TABLE "airbnbs" (
     "id" integer   NOT NULL,
-    "airbnb_name" varchar(255)   NOT NULL,
-    "host_id" integer   NOT NULL,
-    "latitude" decimal   NOT NULL,
-    "longitude" decimal   NOT NULL,
-    "room_id" integer   NOT NULL,
-    "price" decimal   NOT NULL,
-    "minimum_nights" int   NOT NULL,
-    "number_of_reviews" int   NOT NULL,
-    "last_review" date   NOT NULL,
-    "reviews_per_month" decimal   NOT NULL,
-    "availability_365" integer   NOT NULL,
-    "city" varchar(255)   NOT NULL,
+    "airbnb_name" varchar(255)   NULL,
+    "host_id" integer   NULL,
+    "latitude" decimal   NULL,
+    "longitude" decimal   NULL,
+    "room_id" integer   NULL,
+    "price" decimal   NULL,
+    "minimum_nights" int   NULL,
+    "number_of_reviews" int   NULL,
+    "last_review" date   NULL,
+    "reviews_per_month" decimal   NULL,
+    "availability_365" integer  NULL,
+    "city" varchar(255)   NULL,
     CONSTRAINT "pk_airbnbs" PRIMARY KEY (
         "id"
      )
 );
 
 CREATE TABLE "hosts" (
-    "host_id" integer   NOT NULL,
-    "host_name" varchar(255)   NOT NULL,
-    "calculated_host_listings_count" integer   NOT NULL,
+    "host_id" integer   NULL,
+    "host_name" varchar(255)   NULL,
+    "calculated_host_listings_count" integer NULL,
     CONSTRAINT "pk_hosts" PRIMARY KEY (
         "host_id"
      )
@@ -44,7 +44,7 @@ CREATE TABLE "hosts" (
 
 CREATE TABLE "room_types" (
     "room_id" serial   NOT NULL,
-    "room_type" varchar(255)   NOT NULL,
+    "room_type" varchar(255)   NULL,
     CONSTRAINT "pk_room_types" PRIMARY KEY (
         "room_id"
      )
@@ -56,3 +56,44 @@ REFERENCES "hosts" ("host_id");
 ALTER TABLE "airbnbs" ADD CONSTRAINT "fk_airbnbs_room_id" FOREIGN KEY("room_id")
 REFERENCES "room_types" ("room_id");
 
+CREATE TABLE "stg_airbnbs" (
+    "id" integer   NOT NULL,
+    "name" varchar(255)   NULL,
+    "host_id" integer   NULL,
+	"host_name" varchar(255) NULL,
+	"neighborhood_group" varchar(255) NULL,
+	"neighborhood" varchar(25) NULL,
+    "latitude" decimal   NULL,
+    "longitude" decimal   NULL,
+    "room_type" varchar(50)   NULL,
+    "price" decimal   NULL,
+    "minimum_nights" int   NULL,
+    "number_of_reviews" int   NULL,
+    "last_review" date   NULL,
+    "reviews_per_month" decimal   NULL,
+	"calculated_host_listings_count" int null,
+    "availability_365" integer  NULL,
+    "city" varchar(255)   NULL,
+    )
+
+CREATE TABLE "stg_airports" (
+    "iata" varchar(4)   NOT NULL,
+    "airport_name" varchar(255)   NULL,
+    "city" varchar(255)   NULL,
+    "state" varchar(2)   NULL,
+    "latitude" decimal   NOT NULL,
+    "longitude" decimal   NOT NULL
+
+     );
+
+DROP TABLE IF EXISTS stg_airports;
+
+CREATE TABLE stg_airports
+(
+    iata varchar(4) NOT NULL,
+    airport_name varchar(255) NULL,
+    city varchar(255) NULL,
+    state char(2) NULL,
+    latitude numeric NOT NULL,
+    longitude numeric NOT NULL
+)
