563dd2c655af9837abfceba84bcf08814761cf4e
Thomas Lenzmeier <thomas.lenzmeier@constellationmutual.com>
2022-10-12 08:01:22 -0500

    Saving latest work on app.py

--- app.py
+++ app.py
@@ -1,49 +1,94 @@
 # import dependencies
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import jsonify
 import requests
+
+# Import the functions we need from SQL Alchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+# Import any remaining functions
 import json
 
-# import the api keys
-from config import owm_api_key, places_api_key
+# Define the PostgreSQL connection parameters
+username = 'postgres'
+password = 'bootcamp'
+database_name = 'AirBnB'
+port_number = '5432'
+connection_string = f'postgresql://{username}:{password}@localhost:{port_number}/{database_name}'
 
-# set the static OWM api parameters
-owm_param_lang = "en"
+# Connect to the SQL database
+engine = create_engine(connection_string, pool_pre_ping=True)
+base = automap_base()
+base.prepare(engine, reflect=True)
 
-# initialize the flask server
+#Choose the SQL tables we wish to use.
+airbnb = base.classes.airbnbs
+hosts = base.classes.hosts
+#cities = base.classes.us_cities
+
+# Instantiate the Flask application.
 app = Flask(__name__)
-
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # Disable page caching
 
 # landing page
 @app.route("/")
-def index():
-    return render_template("index.html")
+def IndexRoute():
+    webpage = render_template("index.html")
+    return webpage
 
-# weather api call
-@app.route("/weather_forecast/<lat_value>/<lon_value>/<unit_system>/")
-def Weather_Forecast_API_Request(lat_value, lon_value, unit_system):
-    
-    # construct the api url
-    weather_url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat_value}&lon={lon_value}&appid={owm_api_key}&units={unit_system}&lang={owm_param_lang}"
-    
-    # run the api request
-    try:
-        return { "status": "ok", "response": requests.get(weather_url).json() }
-    except requests.exceptions.RequestException as e:
-        return { "status": "not_ok", "response": e.strerror }
+@app.route("/airbnb")
+def AirBnBRoute():
 
-# places api call
-@app.route("/nearby_search/<lat_value>/<lon_value>/<search_radius>/<search_terms>/")
-def Nearby_Places_API_Request(lat_value, lon_value, search_radius, search_terms):
+    session = Session(engine)
+    results = session.query(airbnb.id, airbnb.airbnb_name, airbnb.latitude, airbnb.longitude).all()
+    session.close()
+
+    airbnb_info = []
+    for id, airbnb_name, latitude, longitude in results:
+        dict = {}
+        dict["id"] = id
+        dict["airbnb_name"] = airbnb_name
+        dict["latitude"] = latitude
+        dict["longitude"] = longitude
+        airbnb_info.append(dict)
     
-    # construct the api url
-    places_url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat_value},{lon_value}&radius={search_radius}&type={search_terms}&key={places_api_key}"
-    
-    # run the api request
-    try:
-        return { "status": "ok", "response": requests.get(places_url).json() }
-    except requests.exceptions.RequestException as e:
-        return { "status": "not_ok", "response": e.strerror }
+    return jsonify(airbnb_info)
+
+@app.route("/hosts")
+def HostRoute():
+
+    session = Session(engine)
+    results = session.query(hosts.host_id, hosts.host_name).all()
+    session.close()
+
+    host_info = []
+    for host_id, host_name in results:
+        dict = {}
+        dict["host_id"] = host_id
+        dict["host_name"] = host_name
+        host_info.append(dict)
+        print(host_info)
+    return jsonify(host_info)
+
+# @app.route("/cities")
+# def CityRoute():
+
+#     session = Session(engine)
+#     results = session.query(cities.city_name, cities.latitude, cities.longitude).all()
+#     session.close()
+
+#     city_info = []
+#     for city_name, latitude, longitude in results:
+#         dict = {}
+#         dict["city_name"] = city_name
+#         dict["latitude"] = latitude
+#         dict["longitude"] = longitude
+#         city_info.append(dict)
+#         print(city_info)
+#     return jsonify(city_info)
 
 # run the flask server
 if __name__ == "__main__":
