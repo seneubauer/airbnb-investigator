7b698b8f91fe02a8f628d9d6ede12d401f4046c2
Scott Neubauer <scotteneubauer@gmail.com>
2022-10-12 23:07:45 -0500

    Cleaned code

--- app.py
+++ app.py
@@ -1,51 +1,45 @@
-# import dependencies
+# import dependencies for flask
+from cmath import log
 from flask import Flask
 from flask import render_template
 from flask import jsonify
 import requests
 
-# Import the functions we need from SQL Alchemy
+# import dependencies for sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-# Import any remaining functions
-import json
+# import confidential information
+from config import pg_username, pg_password, pg_port, pg_dbname, owm_api_key, places_api_key
 
-# Define the PostgreSQL connection parameters
-username = 'postgres'
-password = 'bootcamp'
-database_name = 'AirBnB'
-port_number = '5432'
-connection_string = f'postgresql://{username}:{password}@localhost:{port_number}/{database_name}'
+# build the posgresql connection string
+connection_string = f"postgresql://{pg_username}:{pg_password}@localhost:{pg_port}/{pg_dbname}"
 
-# Connect to the SQL database
+# build the sqlalchemy engine
 engine = create_engine(connection_string, pool_pre_ping=True)
+
+# reflect the database schema
 base = automap_base()
 base.prepare(engine, reflect=True)
 
-
-#Choose the SQL tables we wish to use.
+# instantiate the tables required
 airbnb = base.classes.airbnbs
 hosts = base.classes.room_types
 airports = base.classes.airports
-#cities = base.classes.us_cities
+cities = base.classes.us_cities
 
-
-# Instantiate the Flask application.
+# initialize the flask app
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # Disable page caching
+
+# disable page caching
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # landing page
 @app.route("/")
 def IndexRoute():
     webpage = render_template("index.html")
     return webpage
-
-# Populate dropdown from SQL
-@app.route("/query_city_names/")
-def query_city_names():
-
 
 # weather api call
 @app.route("/weather_forecast/<lat_value>/<lon_value>/<unit_system>/")
@@ -73,7 +67,7 @@ def Nearby_Places_API_Request(lat_value, lon_value, search_radius, search_terms)
     except requests.exceptions.RequestException as e:
         return { "status": "not_ok", "response": e.strerror }
     
-
+# get list of dictionaries from airbnbs table
 @app.route("/airbnb")
 def AirBnBRoute():
 
@@ -91,6 +85,7 @@ def AirBnBRoute():
         airbnb_info.append(dict)
     return jsonify(airbnb_info)
 
+# get list of dictionaries from airports table
 # @app.route("/airports")
 # def AirportRoute():
 #     session = Session(engine)
@@ -107,6 +102,7 @@ def AirBnBRoute():
 #         airport_info.append(dict)
 #     return jsonify(airport_info) 
 
+# get list of dictionaries from cities table
 # @app.route("/cities")
 # def CityRoute():
 
